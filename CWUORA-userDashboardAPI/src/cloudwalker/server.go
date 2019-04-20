012d04b5643c5043e94bfc2193e157b506991dc5
Yueqiao Zhang <yueqiaozhang79@gmail.com>
2019-04-20 13:36:10 -0700

    Able to request to get space,question and answer data and insert into mongdb

--- CWUORA-userDashboardAPI/src/cloudwalker/server.go
+++ CWUORA-userDashboardAPI/src/cloudwalker/server.go
@@ -51,8 +51,19 @@ func initRoutes(mx *mux.Router, formatter *render.Render) {
 func homeHandler(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		//params := mux.Vars(req)
-		var spaceid string = "5cb3c8ab78163fa3c9726fb3"
-		/* store all info, fetch data from david and store in mongodb */
+		//var spaceid string = "5cb3c8ab78163fa3c9726fb3"
+		/*************Mongo server setup*******************/
+		session, err := mgo.Dial(mongodb_server)
+        if err != nil {
+                fmt.Println("mongoserver panic")
+        }
+        defer session.Close()
+        session.SetMode(mgo.Monotonic, true)
+        ms := session.DB(mongodb_database).C("space")
+        mq := session.DB(mongodb_database).C("question")
+        ma := session.DB(mongodb_database).C("answer")
+
+		/************** store all info, fetch data from david and store in mongodb ******/
 		resp, err := http.Get("http://34.217.213.85:3000/msgstore/v1/spaces?depth=2")
 		if err != nil {
 			log.Fatalln(err)
@@ -62,93 +73,109 @@ func homeHandler(formatter *render.Render) http.HandlerFunc {
 		if err != nil {
 			log.Fatalln(err)
 		}
-		/********************************************************/
-		var depth1 []Depth1
-		json.Unmarshal(body, &depth1)
-		//fmt.Println(depth1)
-		// get homepage followed spaces
-		spaces := []*MongoInfoSpace{}
-		space := new(MongoInfoSpace)
-		
 
-		for i := 0; i < len(depth1); i++ {
-			var id = depth1[i].Id
-			var title = depth1[i].Title
-			space = new(MongoInfoSpace)
-			space.Id = id
-			space.Title = title
-			fmt.Println("depth1", i)
-			fmt.Println("title", title)
-			//fmt.Println("title", depth1[i].Questions)
-			// create question list for space
-			var count = len(depth1[i].Questions)
-			//questions := [count]SpaceQuestions{}
-			fmt.Println("count", count)
-			questions := make([]SpaceQuestions, count)
-			//sQuestions := new(SpaceQuestions)
+		var spaceAllContent []SpaceContentAPI
+		json.Unmarshal(body, &spaceAllContent)
+		/**************** Convert data to mongo ****************************************/
+		/* create mongo space struct */
+		mspaces := []*MSpace{}
+		mspace := new(MSpace)
+
+		for i := 0; i < len(spaceAllContent); i++ {
+
+			mspace = new(MSpace)
+			mspace.Id = spaceAllContent[i].Id
+			mspace.Title = spaceAllContent[i].Title
+
+			/******** create mongo question struct *********/
+			var count = len(spaceAllContent[i].Questions)
+			mquestions := make([]MQuestion, count)
+
+			var qcontent []QuestionContentAPI
+			qcontent = spaceAllContent[i].Questions
+
 			for j := 0; j < count; j++ {
-				questions[j].Id = depth1[i].Questions[j].Id
+				mquestions[j].Id = qcontent[j].Id
+				mquestions[j].Body = qcontent[j].Body
+				mquestions[j].CreatedOn = qcontent[j].CreatedOn
+
+				/******** create mongo answer struct *********/
+				var acount = len(qcontent[j].Answers)
+				manswers := make([]MAnswer, acount)
+
+				var acontent []AnswerContentAPI
+				acontent = qcontent[j].Answers
+
+				for k := 0; k < acount; k++ {
+					manswers[k].Id = acontent[k].Id
+					manswers[k].Body = acontent[k].Body
+					manswers[k].CreatedOn = acontent[k].CreatedOn
+					/*********insert answer data into mongo*******************/
+					ma.Insert(manswers[k])
+				}
+
+				mquestions[j].Answers = manswers
+				/*********insert question data into mongo*******************/
+				mq.Insert(mquestions[j])
 			}
-			space.Questions = questions
-			spaces = append(spaces, space)
+
+			mspace.Questions = mquestions
+			/*********insert space data into mongo*******************/
+			ms.Insert(mspace)
+			mspaces = append(mspaces, mspace)
 		}
 
 
-		// insert content to mongo
-		session, err := mgo.Dial(mongodb_server)
-        if err != nil {
-                fmt.Println("mongoserver panic")
-        }
-        defer session.Close()
-        session.SetMode(mgo.Monotonic, true)
+		// // insert content to mongo
+		
         // insert space 
-        c := session.DB(mongodb_database).C("space")
-        for i := 0; i < len(spaces); i++ {
-         	c.Insert(spaces[i])
-        }
-        // find id from mongodb, using space id to find question id
-        //convert space id to mongo id
-        bsonObjectID := bson.ObjectIdHex(spaceid)
+        // c := session.DB(mongodb_database).C("space")
+        // for i := 0; i < len(spaces); i++ {
+        //  	c.Insert(spaces[i])
+        // }
+        // // find id from mongodb, using space id to find question id
+        // //convert space id to mongo id
+        // bsonObjectID := bson.ObjectIdHex(spaceid)
 
-        var spaceResult bson.M
-		err = c.Find(bson.M{"_id": bsonObjectID}).One(&spaceResult)
-		if err != nil {
-			fmt.Println("findquery panic")
-		}
+  //       var spaceResult bson.M
+		// err = c.Find(bson.M{"_id": bsonObjectID}).One(&spaceResult)
+		// if err != nil {
+		// 	fmt.Println("findquery panic")
+		// }
 
-	    var selectQuestions []interface{}
-	    selectQuestions = spaceResult["questions"].([]interface{})
-	    var qsCount int = len(selectQuestions)
-	    fmt.Println(qsCount)
-	    // add question to frontend
+	 //    var selectQuestions []interface{}
+	 //    selectQuestions = spaceResult["questions"].([]interface{})
+	 //    var qsCount int = len(selectQuestions)
+	 //    fmt.Println(qsCount)
+	 //    // add question to frontend
 
-	    var result bson.M
-	    result = selectQuestions[0].(bson.M)
+	 //    var result bson.M
+	 //    result = selectQuestions[0].(bson.M)
 	  
-	    // convert interface to array
-	    fmt.Println("result", result["_id"])
+	 //    // convert interface to array
+	 //    fmt.Println("result", result["_id"])
 
-	    // // generate frontend json
-	    var response Home
-	    //var resSpace []SpaceAPI
-	    resSpace := make([]SpaceAPI, 1)
-	    resQuestions := make([]QuestionAPI, qsCount)
-	    // add content to resSpace
-	    resSpace[0].Id = spaceResult["_id"].(bson.ObjectId)
-	    resSpace[0].Title = spaceResult["title"].(string)
+	 //    // // generate frontend json
+	 //    var response Home
+	 //    //var resSpace []SpaceAPI
+	 //    resSpace := make([]SpaceAPI, 1)
+	 //    resQuestions := make([]QuestionAPI, qsCount)
+	 //    // add content to resSpace
+	 //    resSpace[0].Id = spaceResult["_id"].(bson.ObjectId)
+	 //    resSpace[0].Title = spaceResult["title"].(string)
 
-	    // add content to resQuestions
-	    for i := 0; i < qsCount; i++ {
-	    	var r bson.M
-	    	r = selectQuestions[i].(bson.M)
-	    	resQuestions[i].Id = r["_id"].(bson.ObjectId)
-	    }
+	 //    // add content to resQuestions
+	 //    for i := 0; i < qsCount; i++ {
+	 //    	var r bson.M
+	 //    	r = selectQuestions[i].(bson.M)
+	 //    	resQuestions[i].Id = r["_id"].(bson.ObjectId)
+	 //    }
 
-	    response.SpaceAPIs = resSpace
-	    response.QuestionAPIs = resQuestions
+	 //    response.SpaceAPIs = resSpace
+	 //    response.QuestionAPIs = resQuestions
 
 				
-		formatter.JSON(w, http.StatusOK, response)
+		formatter.JSON(w, http.StatusOK, mspaces)
 		//formatter.JSON(w, http.StatusOK, spaceResult)
 	}
 }
@@ -232,15 +259,41 @@ db.space.insert(
 		_id: 12345,
 		title: "spacecontenttest",
 		questions: [
-			{_id: 123456},
-			{_id: 234566} ]
+			{
+				_id: 123456,
+				questionText: "testquestion",
+				createdOn: "0001-01-01T00:00:00Z"
+				answers: [
+					{
+						_id: 12344,
+						answerText: "answertest",
+						createdOn: "0001-01-01T00:00:00Z"
+					}
+				]
+			}
+		]
 	}
 );
 
 db.quesiton.insert(
 	{
-		_id: 23456,
-		body: "what is cs",
+		_id: 123456,
+		questionText: "testquestion",
+		createdOn: "0001-01-01T00:00:00Z"
+		answers: [
+			{
+				_id: 12344,
+				answerText: "answertest",
+				createdOn: "0001-01-01T00:00:00Z"
+			}
+		]
+	}
+);
+
+db.answer.insert(
+	{
+		_id: 12344,
+		answerText: "answertest",
 		createdOn: "0001-01-01T00:00:00Z"
 	}
 );
--- CWUORA-userDashboardAPI/src/cloudwalker/types.go
+++ CWUORA-userDashboardAPI/src/cloudwalker/types.go
@@ -5,11 +5,7 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-type userProfile struct {
-	userId          	string    `json:"userId" bson:"userId"`	
-	uspaces		        []SpaceAPI  `json:"followedspaces" bson:"followedspaces"`
-	uquestions          []QuestionAPI `json:"followedquestions" bson:"followedquestions"`
-}
+
 
 // type Space struct {
 // 	SpaceId         string     `json:"spaceId"` 
@@ -32,9 +28,15 @@ type Answer struct {
 	Content			string   `json:"content"` 
 }
 
+/******************Frontend Part**************************/
+//home page response type
+type Home struct {
+	SpaceAPIs		[]SpaceAPI `json:"followed_topics"`
+	QuestionAPIs	[]QuestionAPI `json:"my_questions"`
+}
+
 type SpaceAPI struct {
 	Id 				bson.ObjectId 	`json:"_id" bson:"_id"`
-	//SpaceId         string	`json:"id"` 
 	Title			string	`json:"name"`
 }
 
@@ -44,23 +46,17 @@ type QuestionAPI struct {
 	CreatedOn		time.Time  `json:"createdOn"`
 }
 
-//home page response type
-type Home struct {
-	SpaceAPIs		[]SpaceAPI `json:"followed_topics"`
-	QuestionAPIs	[]QuestionAPI `json:"my_questions"`
-}
-
 // get space content from space server
+// type SpaceContentAPI struct {
+// 	Id  		bson.ObjectId 	`json:"_id" bson:"_id"`
+//     Title 		string 			`json:"title"`
+//     CreatedOn 	time.Time 		`json:"createdOn"`
+//     Description string 			`json:"description"`
+//     Tags 		[]SpaceTags 	`json:"tags"`
+// }
+
+/******************David Part**************************/
 type SpaceContentAPI struct {
-	Id  		bson.ObjectId 	`json:"_id" bson:"_id"`
-    Title 		string 			`json:"title"`
-    CreatedOn 	time.Time 		`json:"createdOn"`
-    Description string 			`json:"description"`
-    Tags 		[]SpaceTags 	`json:"tags"`
-    //Questions	[]QuestionContentAPI  `json:"questions"`
-}
-// get Depth1 content from questions server
-type Depth1 struct {
 	Id  		bson.ObjectId 	`json:"_id" bson:"_id"`
     Title 		string 			`json:"title"`
     CreatedOn 	time.Time 		`json:"createdOn"`
@@ -73,27 +69,49 @@ type SpaceTags struct {
 	Tag 		string			`json:"tag"`
 }
 
-// get question content from question server
+// get question content from david
 type QuestionContentAPI struct {
     Id  		bson.ObjectId 	`json:"_id" bson:"_id"`
 	SpaceId 	bson.ObjectId 	`json:"spaceId" bson:"spaceId"`
     Body 		string 			`json:"questionText"`
     CreatedOn 	time.Time 		`json:"createdOn"`
-    //CreatedBy 	bson.ObjectId 	`json:"createdBy" bson:"createdBy"`
+    CreatedBy 	string	`json:"createdBy" bson:"createdBy"`
+    Answers	    []AnswerContentAPI  `json:"answers"`
+}
+
+// get answer content from david
+type AnswerContentAPI struct {
+    Id  		bson.ObjectId 	`json:"_id" bson:"_id"`
+	QuestionId 	bson.ObjectId 	`json:"questionId" bson:"questionId"`
+    Body 		string 			`json:"answerText"`
+    CreatedOn 	time.Time 		`json:"createdOn"`
     CreatedBy 	string	`json:"createdBy" bson:"createdBy"`
 }
+/****************** Mongo Part**************************/
 
-// insert data into mongo struct
-type MongoInfoSpace struct {
+// db.space
+type MSpace struct {
 	Id  		bson.ObjectId 	`json:"_id" bson:"_id"`
 	Title 		string 			`json:"title"`
-	Questions 	[]SpaceQuestions 	`json:"questions"`
+	Questions 	[]MQuestion 	`json:"questions"`
 }
-
-type SpaceQuestions struct {
+// db.question
+type MQuestion struct {
 	Id  		bson.ObjectId 	`json:"_id" bson:"_id"`
 	Body 		string			`json:"questionText"`
+	CreatedOn 	time.Time 		`json:"createdOn"`
+	Answers     []MAnswer		`json:"answers"`
 }
-
-var Spaces [] Space
+// db.answer
+type MAnswer struct {
+	Id  		bson.ObjectId 	`json:"_id" bson:"_id"`
+	Body 		string			`json:"answerText"`
+	CreatedOn 	time.Time 		`json:"createdOn"`
+}
+// db.user
+type MUserProfile struct {
+	userId          	string    `json:"userId" bson:"userId"`	
+	uspaces		        []SpaceAPI  `json:"followedspaces" bson:"followedspaces"`
+	uquestions          []QuestionAPI `json:"followedquestions" bson:"followedquestions"`
+}
 

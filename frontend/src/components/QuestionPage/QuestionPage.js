aaef5f440f6083ea9d6be634ab23f035d2fa178f
Yu Zhao <yu.zhao@sjsu.edu>
2019-04-23 00:55:43 -0700

    quesionPage Layout

--- frontend/src/components/QuestionPage/QuestionPage.js
+++ frontend/src/components/QuestionPage/QuestionPage.js
@@ -4,19 +4,148 @@ import cookie from 'react-cookies';
 //import { Redirect } from 'react-router';
 import { userActions } from '../../_actions';
 import { connect } from 'react-redux';
-import { Container, Col, Card, Button } from 'react-bootstrap';
+import { ListGroup, Container, ListGroupItem } from 'react-bootstrap';
 import ReactPaginate from 'react-paginate';
+import Moment from 'react-moment';
+import PropTypes from 'prop-types';
+
+const fake_reponse = {
+    "spaceId": "spaceId",
+    "createdBy": "createdBy",
+    "topics": [{
+        "topics": {
+            "label": "label"
+        }
+    }, {
+        "topics": {
+            "label": "label"
+        }
+    }],
+    "answers": [{
+        "answers": {
+            "questionId": "questionId",
+            "comments": [{
+                "replies": {
+                    "answerId": "answerId",
+                    "replies": [{}, {}],
+                    "createdBy": "createdBy",
+                    "parentCommentId": "parentCommentId",
+                    "_id": "_id",
+                    "commentText": "commentText",
+                    "createdOn": "2000-01-23T04:56:07.000+00:00"
+                }
+            }, {
+                "replies": {
+                    "answerId": "answerId",
+                    "replies": [{}, {}],
+                    "createdBy": "createdBy",
+                    "parentCommentId": "parentCommentId",
+                    "_id": "_id",
+                    "commentText": "commentText",
+                    "createdOn": "2000-01-23T04:56:07.000+00:00"
+                }
+            }],
+            "answerText": "answerText",
+            "createdBy": "createdBy",
+            "_id": "_id",
+            "createdOn": "2000-01-23T04:56:07.000+00:00"
+        }
+    }, {
+        "answers": {
+            "questionId": "questionId",
+            "comments": [{
+                "replies": {
+                    "answerId": "answerId",
+                    "replies": [{}, {}],
+                    "createdBy": "createdBy",
+                    "parentCommentId": "parentCommentId",
+                    "_id": "_id",
+                    "commentText": "commentText",
+                    "createdOn": "2000-01-23T04:56:07.000+00:00"
+                }
+            }, {
+                "replies": {
+                    "answerId": "answerId",
+                    "replies": [{}, {}],
+                    "createdBy": "createdBy",
+                    "parentCommentId": "parentCommentId",
+                    "_id": "_id",
+                    "commentText": "commentText",
+                    "createdOn": "2000-01-23T04:56:07.000+00:00"
+                }
+            }],
+            "answerText": "answerText",
+            "createdBy": "createdBy",
+            "_id": "_id",
+            "createdOn": "2000-01-23T04:56:07.000+00:00"
+        }
+    }],
+    "_id": "_id",
+    "createdOn": "2000-01-23T04:56:07.000+00:00",
+    "questionText": "questionText"
+}
 
 
-const Answer = (
+export class AnswerList extends Component {
+    static propTypes = {
+        data: PropTypes.array.isRequired,
+    };
+    render() {
+        let details = null;
+        if (this.props.data.length != 0) {
+            details = this.props.data.map(post => {
+                return (
+                    <ListGroup.Item>
+                        <ul class="list-unstyled">
+                            <li>{post.answers.createdBy}</li>
+                            <li><small class="text-muted">Answered<Moment fromNow>{post.answers.createdOn}</Moment></small></li>
+                        </ul>
+                        <p>
+                            {post.answers.answerText}
+                        </p>
+                    </ListGroup.Item>
 
-)
+                )
+            });
+        }
+        else {
+            details = (<div>No Answer Yet</div>)
+        }
+        return (
+            <ListGroup variant="flush">
+                {details}
+            </ListGroup>
+        )
+    }
+}
 
-const AnswerList = 
+
+
 class QuestionPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
 
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Container>
+                    <ListGroup variant="flush">
+                        <ListGroupItem>
+                            <h4><b>{fake_reponse.questionText}</b></h4>
+                        </ListGroupItem>
+                    </ListGroup>
+
+                    <AnswerList data={fake_reponse.answers} />
+                </Container>
+            </div>
+        )
+    }
 
 }
 
-const mapStateToProps = ({authentication}) => ({authentication});
+const mapStateToProps = ({ authentication }) => ({ authentication });
 export default connect(mapStateToProps)(QuestionPage);

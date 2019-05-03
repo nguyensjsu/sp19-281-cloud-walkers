837a94c9e12a4be348a1460983b4d76f9ce8eada
Yu Zhao <yu.zhao@sjsu.edu>
2019-05-02 23:29:43 -0700

    config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -3,11 +3,12 @@ var backend_host = 'http://ec2-34-209-245-156.us-west-2.compute.amazonaws.com:80
 
 //var user_tracking_apis = 'http://34.222.10.95:3000';
 var user_tracking_apis = 'http://34.221.236.225:8000/activityapi'
-var david_test_apis = "http://54.190.23.212:3000/msgstore/v1";
+//var david_test_apis = "http://54.190.23.212:3000/msgstore/v1";
+var david_test_apis = 'http://34.221.236.225:8000/msgstore/v1'
 
-var msgstore_apis = 'http://35.164.157.104:8000/msgstore/v1';
+//var msgstore_apis = 'http://35.164.157.104:8000/msgstore/v1';
 
 //var user_auth_apis = 'http://54.200.4.62:3001';
 var user_auth_apis = 'http://34.221.236.225:8000/authapi';
 
-export {backend_host, user_tracking_apis, msgstore_apis, david_test_apis, user_auth_apis};
+export {backend_host, user_tracking_apis, david_test_apis, user_auth_apis};
